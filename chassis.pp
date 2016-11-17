57d85d2525e175e0b67add00738753f387b724ec
Bronson Quick <bronson@sennza.com.au>
2016-11-17 20:59:41 +1000

    Add a unique variable

--- chassis.pp
+++ chassis.pp
@@ -1,6 +1,6 @@
-$chassis_config = sz_load_config()
+$fish_config = sz_load_config()
 
 class { 'fish':
   path   => '/vagrant/extensions/fish',
-  config => $chassis_config
+  config => $fish_config
 }
